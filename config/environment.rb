71b75b015dc20072b6c2567d6e420acf6befbf89
Mike <mclancy96@gmail.com>
2025-08-04 10:17:35 -0400

    Remove gem versions and install necessary dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
-ENV['RACK_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= 'development'
+
+# Require logger first for Ruby 3.3+ compatibility
+require 'logger'
 
 # Require in Gems
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require(:default, ENV.fetch('RACK_ENV', nil))
 
 # Require in all files in 'app' directory
 require_all 'app'
