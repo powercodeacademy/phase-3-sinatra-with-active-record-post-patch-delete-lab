a1ebe8dc8328e6838eca62ca0906c5acc87cef2f
Mike <mclancy96@gmail.com>
2025-09-18 16:54:50 -0400

    Add binstubs, update to 3.3.5, and update README accordingly

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -5,7 +5,6 @@ class BakedGood < ActiveRecord::Base
   # https://guides.rubyonrails.org/active_record_querying.html#scopes
   # scope :by_price, -> { order(price: :desc) }
   def self.by_price
-    self.all.order(price: :desc)
+    all.order(price: :desc)
   end
-
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
-require_relative 'config/environment'
-require 'rack/contrib'
+require_relative "config/environment"
+require "rack/contrib"
 
 # Parse JSON from the request body into the params hash
 use Rack::JSONBodyParser
